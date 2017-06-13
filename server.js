cabcb7d383c505e2acbb2bd9f0384f4cdabed50e
marsbaa <ray@marsbaa.com>
2017-06-13 10:09:35 +1000

    server settings

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
+app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
     next();
   }
 });
-
+app.get('*', function(request, res) {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log('Express server is up on port ' + PORT);
 });
