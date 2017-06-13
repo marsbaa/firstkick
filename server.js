f9df6fd30d46505eaa3b932b1b0a9c8dc1d01af2
marsbaa <ray@marsbaa.com>
2017-06-13 10:25:49 +1000

    server.js update

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ var path = require('path');
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 app.get('*', function(request, res) {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
